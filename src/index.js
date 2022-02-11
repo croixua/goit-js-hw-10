82800372abdc6eb7890012b333371774129c6762
Anton Kit <croixua@gmail.com>
2022-02-11 23:02:29 +0200

    add template country card

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+import debouce from 'lodash.debounce';
 import './css/styles.css';
+import countryCard from './template/country-card.hbs';
+import fetchCountries from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+fetch('https://restcountries.com/v3.1/name/peru')
+  .then(response => response.json())
+  .then(country => {
+    console.log(country[0].languages);
+    const markup = countryCard(country);
+    console.log(markup);
+  });
+
+// console.log(countryCard());
+
+// fetch('https://restcountries.com/v3.1/name/peru?')
+//   .then(response => response.json())
+//   .then(country => {
+//     console.log(country[0].languages);
+//     const markup = countryCard(country[0]);
+//     console.log(markup);
+//   });
