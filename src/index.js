6692c1d67b4a4c5a5b9b5310ca66ebd7851f22b5
Anton Kit <croixua@gmail.com>
2022-02-12 20:33:22 +0200

    Named import fetchCountries()

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
 import debouce from 'lodash.debounce';
 import './css/styles.css';
 import countryCard from './template/country-card.hbs';
-import fetchCountries from './js/fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
 
 const inputRef = document.querySelector('#search-box');
 const countryListRef = document.querySelector('.country-list');
 const countryInfoRef = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
-const BASE_URL_NAME = 'https://restcountries.com/v3.1/';
 
 inputRef.addEventListener('input', onInput);
 
 function onInput(e) {
   const name = e.target.value;
 
-  // fetchCountries(name);
-
-  fetch(`${BASE_URL_NAME}name/${name}`)
-    .then(response => response.json())
-    .then(country => {
-      const markup = countryCard(country);
-
-      countryInfoRef.innerHTML = markup;
-    });
+  fetchCountries(name, countryInfoRef);
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,8 @@
-const BASE_URL_NAME = 'https://restcountries.com/v3.1/';
+import countryCard from '../template/country-card.hbs';
+const BASE_URL_NAME = 'https://restcountries.com/v3.1/name/';
 
-export const fetchCountries = name => {
-  fetch(`${BASE_URL_NAME}name/${name}`)
+export const fetchCountries = (name, countryInfoRef) => {
+  fetch(`${BASE_URL_NAME}${name}`)
     .then(response => response.json())
     .then(country => {
       const markup = countryCard(country);
